8ef435b5790be9f91e36f4be7d5f31746e1099fe
Luis Simas <luissimas@protonmail.com>
2021-06-14 10:04:36 -0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+/*
+
+Autor(es): Luís Augusto Simas do Nascimento
+Data de Criação: 14/06/2021
+Data de Atualização: 14/06/2021
+
+Objetivos: Trabalho de AED1
+
+*/
+
+// Bibliotecas
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// Função principal
+int main(int argc, char *argv[])
+{
+
+  return 0;
+}
