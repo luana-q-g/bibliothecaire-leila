4fd1a25b0f9e1770fbde764f619c83061ce0b9f0
Luis Simas <luissimas@protonmail.com>
2021-06-23 08:24:17 -0300

    Classe para os botões
    Agora os botões estão encapsulados em uma classe. Acredito que fica mais
    simples de se trabalhar com eles dessa forma (e era algo necessário,
    pois vamos precisar implementar o comportamento de click para cada um deles)

---
+++ button/button.cpp
@@ -0,0 +1,30 @@
+/*
+
+  Autor(es): Luís Augusto Simas do Nascimento
+  Data de Criação: 23/06/2021
+  Data de Atualização: 23/06/2021
+
+  Objetivos: Implementar a classe button
+
+*/
+
+#include <iostream>
+#include <string>
+#include "button.h"
+
+using namespace std;
+
+
+Button::Button(string tipo) : sf::RectangleShape(sf::Vector2f(112,56)){
+  if(!textureIdle.loadFromFile("./interface/assets/imagens/botoes/botoes_" + tipo + ".png")
+  || !textureClicked.loadFromFile("./interface/assets/imagens/botoes_clicados/botoes_clicados_" + tipo + ".png")){
+    cout << "Erro: não foi possível carregar a imagem do botão " + tipo << endl;
+  }else{
+    setTexture(&textureIdle);
+    //setPosition(440, 600);
+  }
+}
+
+bool Button::isInside(const sf::Vector2i){
+  return true;
+}
---
+++ button/button.h
@@ -0,0 +1,46 @@
+/*
+
+  Autor(es): Luís Augusto Simas do Nascimento
+  Data de Criação: 23/06/2021
+  Data de Atualização: 23/06/2021
+
+  Objetivos: Arquivo de cabeçalho da classe button
+
+*/
+
+#ifndef BUTTON_H
+#define BUTTON_H
+
+#include <SFML/Graphics/RectangleShape.hpp>
+#include <SFML/Graphics/Texture.hpp>
+#include <SFML/System/Vector2.hpp>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+enum botoes {
+  historia,
+  psicologia,
+  letras,
+  filosofia,
+  geografia,
+  matematica,
+  informatica,
+  quimica,
+  arte
+};
+
+class Button : public sf::RectangleShape {
+private:
+  bool clicked;
+  bool active;
+  sf::Texture textureClicked;
+  sf::Texture textureIdle;
+
+public:
+  Button(string tipo);
+  bool isInside(const sf::Vector2i);
+};
+
+#endif
